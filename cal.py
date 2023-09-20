0734e27cacc5c21e6f372650462c8fe8ba85a0bd
Husnain-Ali-24 <husnainrajput246@gmail.com>
2023-09-19 23:37:52 -0700

    initial commit

---
+++ cal.py
@@ -0,0 +1,16 @@
+Input1=int(input("Enter First value "))
+Input2=int(input("Enter Second value "))
+print("Press 1 for Addition\nPress 2 for subtraction\nPress 3 for Multiplication\nPress 4 for Division")
+Input3=int(input("Enter Choice "))
+if Input3==1:
+ result=Input1+Input2
+ print(result)
+if Input3==2:
+ result=Input1-Input2
+ print(result)
+if Input3==3:
+ result=Input1*Input2
+ print(result)
+if Input3==4:
+ result=Input1/Input2
+ print(result)
